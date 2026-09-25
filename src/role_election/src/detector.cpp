88357994e0df33139a23fc8bb9bbc36d6bc9a1ff
Arthur Albuquerque <aas13@cin.ufpe.br>
2026-09-24 22:51:38 -0300

    .

--- src/role_election/src/detector.cpp
+++ src/role_election/src/detector.cpp
@@ -6,11 +6,13 @@
 
 class Detector : public rclcpp::Node {
 public:
+    // Construtor da classe Detector
     Detector() : Node("detector") {
 
-        // Declara o parâmetro que identifica o robô
+        // Declara o parâmetro "robot_id" com valor padrão 1
         this->declare_parameter<int>("robot_id", 1);
 
+        // Obtém o valor do parâmetro "robot_id" e armazena na variável robot_id_
         robot_id_ = this->get_parameter("robot_id").as_int();
 
         // Publica todas as detecções no mesmo tópico
@@ -29,14 +31,15 @@ private:
         msg.robot_id = robot_id_;
         msg.distance = distance_distribution_(generator_);
 
-        publisher_->publish(msg);
-
+        
         RCLCPP_INFO(this->get_logger(), "Robô %d | Distância até a bola: %.2f", msg.robot_id, msg.distance);
-
+        
         publisher_count_++;
         if (publisher_count_ == 3) {
             sleep(1);  // Aguarda 1 segundo antes de publicar novamente
+            publisher_count_ = 0;
         }
+        publisher_->publish(msg);
     }
 
     int robot_id_;
